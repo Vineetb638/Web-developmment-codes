f8e5d3fa53702142f29845a44cf06e7cc08664ab
Vineet <vineetb638@gmail.com>
2024-10-24 00:31:16 +0530

    const , var and let

---
+++ Javascript/01_basics.js
@@ -0,0 +1,16 @@
+const accountID = 144202
+let accountEmail = "vineet@baghel.in"
+var accountPassword = "12345"
+accountCity = "Agra"
+/*
+1. we can not change the value of caonst variable 
+2. we should not use var because of the functional scope 
+*/
+accountEmail = "vivek@.in"
+accountPassword = "jhkssd"
+accountCity = "Mathura"
+console.log(accountEmail);
+console.table([accountID , accountEmail , accountPassword , accountCity])
+/* console.log is used for just print the data 
+and console.table is used for printing the multiple data in one table
+*/
