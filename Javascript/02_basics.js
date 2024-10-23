107d0d53ae68cbae11ae2cf229afd66be677091f
Vineet <vineetb638@gmail.com>
2024-10-24 00:51:52 +0530

    datypes

---
+++ Javascript/02_basics.js
@@ -0,0 +1,19 @@
+"use strict"
+/*
+Use strict is used when we have to make all the codes newer
+*/
+let isLogged = false;
+let state
+/*
+Here we are study about the datatype
+1. number => 2 to power 53
+2. bigint =>
+3. string => ""
+4. null => standalone value;
+5. undefined => 
+6. symbol => unique
+7. object
+*/
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "vineet");
